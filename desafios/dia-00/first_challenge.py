d227142706ad63eeaaa49da50741512c1e1e26f6
PedroBuarque <pedroh.buarque@gmail.com>
2024-01-11 11:55:14 -0300

    Add first challenge

---
+++ desafios/dia-00/first_challenge.py
@@ -0,0 +1,7 @@
+user_input = input('Type a number: ')
+int_user_input = int(user_input)
+
+if int_user_input % 2 == 0:
+    print('The number you typed is a pair number')
+else:
+    print('The number you typed is a odd number')
