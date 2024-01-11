fd6d7c2529443ac624c3b89bcd9ddf3cb6fedcf7
PedroBuarque <pedroh.buarque@gmail.com>
2024-01-11 12:08:46 -0300

    Enhance code using f-strings

--- solucoes/dia-00/first_challenge.py
+++ solucoes/dia-00/first_challenge.py
@@ -2,6 +2,6 @@ user_input = input('Type a number: ')
 int_user_input = int(user_input)
 
 if int_user_input % 2 == 0:
-    print('The number you typed is a pair number')
+    print(f'{int_user_input} is a pair number')
 else:
-    print('The number you typed is a odd number')
+    print(f'{int_user_input} is a odd number')
